7cabc52db2865d6a041f3eb33880280ac0279a70
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-04 23:53:57 +0530

    Performing push operation over Stack using linked list

--- src/stack/StackLinkedList.java
+++ src/stack/StackLinkedList.java
@@ -1,12 +1,20 @@
 package stack;
 
 
+import java.util.Scanner;
+
 public class StackLinkedList {
     Node top;
+    static Scanner sc = new Scanner(System.in);
     StackLinkedList(){
         top = null;
     }
     public void push(){
+        int data = sc.nextInt();
+
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.next=top;
 
     }
 }
