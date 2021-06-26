b8634e16f99b274d8c41aebb1e86ec34c244f723
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-26 23:31:15 +0530

    Implementing main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,12 +7,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         StackLinkedList obj1 = new StackLinkedList();
         StackArray obj2 = new StackArray();
         System.out.println("Stack using :-");
         System.out.println("Press 1 for LinkedList");
         System.out.println("Press 2 for Array \n");
         System.out.print("Your choice: ");
+        int c = scanner.nextInt();
         while(true){
             System.out.println("-------------------------------------------------------------");
             System.out.println("Press 1 for push");
@@ -21,7 +23,7 @@ public class Main {
             System.out.println("Press 4 to exit");
             System.out.println("-------------------------------------------------------------");
             System.out.println("Enter your choice");
-            Scanner scanner = new Scanner(System.in);
+
             int choice = scanner.nextInt();
             switch (choice){
                 case 1:
