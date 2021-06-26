c62d99fd2e5ddf3c7f1e3a8135ae573644e2ed2f
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-26 23:31:25 +0530

    Implementing main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,35 +15,6 @@ public class Main {
         System.out.println("Press 2 for Array \n");
         System.out.print("Your choice: ");
         int c = scanner.nextInt();
-        while(true){
-            System.out.println("-------------------------------------------------------------");
-            System.out.println("Press 1 for push");
-            System.out.println("Press 2 for pop");
-            System.out.println("Press 3 for traversal");
-            System.out.println("Press 4 to exit");
-            System.out.println("-------------------------------------------------------------");
-            System.out.println("Enter your choice");
 
-            int choice = scanner.nextInt();
-            switch (choice){
-                case 1:
-                    //obj1.push();
-                    //obj2.push();
-                    break;
-                case 2:
-                    //obj1.pop();
-                    //obj2.pop();
-                    break;
-                case 3:
-                    //obj1.traverse();
-                    //obj2.traverse();
-                    break;
-                case 4:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Wrong Choice !!!");
-            }
-        }
     }
 }
